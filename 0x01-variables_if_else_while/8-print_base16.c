444d56ebd948255b61c302c2d7b91b45d88c2ce0
Dagem587 <dagem1989@gmail.com>
2022-09-09 13:23:45 +0000

    numbers and letters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 	int n;
 	char al;
 
-	for (n = 0 ; n <= 9 ; n++)
+	for (n = 48 ; n <= 57 ; n++)
 	{
 		putchar(n);
 	}
-	for (al = 'a' ; al <= 'f' ; al++)
+	for (al = 97 ; al <= 102 ; al++)
 	{
 		putchar(al);
 	}
