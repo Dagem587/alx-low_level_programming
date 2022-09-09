d5b1dd68bf6c703165a01a288e8f17de11ee655e
Dagem587 <dagem1989@gmail.com>
2022-09-09 12:46:42 +0000

    single digit from zero to nine using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - the entry  point for the program
+ *
+ * Description: The program prints numbers from zero to nine
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
