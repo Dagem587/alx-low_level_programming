669081594441b69fefd2a032638193a2fe37d77c
Dagem587 <dagem1989@gmail.com>
2022-09-13 05:23:41 +0000

    print lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char al;
+
+	for (al = 'a' ; al <= 'z' ; al++)
+		putchar(al);
+	putchar('\n');
+
+	return (0);
+}
